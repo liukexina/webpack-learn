c688fc8471ef1ee77348b6098e81b0d718f1034a
liukexin08 <liukexin08@maoyan.com>
2021-02-19 10:53:47 +0800

    开发环境配置

---
+++ 08-开发环境配置/webpack.config.js
@@ -0,0 +1,59 @@
+const {resolve}  = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'index.js',
+    path: resolve(__dirname, 'build'),
+    publicPath: './'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, 
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      }, 
+      {
+        test: /\.(jpg|png|jpeg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          esModule: false,
+          name: '[hash:10].[ext]',
+          outputPath: 'img'
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        exclude: /\.(css|js|html|jpg|png|jpeg|less)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]',
+          outputPath: 'fonts'
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+  mode: 'development',
+  devServer: {
+    // 项目构建后的路径
+    contentBase: resolve(__dirname, 'build'),
+    // 开启gzip压缩
+    compress: true,
+    // 端口号
+    port: 8080,
+    // 自动打开浏览器
+    open: true,
+  }
+}
