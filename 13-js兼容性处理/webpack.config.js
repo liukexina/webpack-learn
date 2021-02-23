fa21dc101c848278a2c80153e3b729cd878ea417
刘可新 <liukexin08@maoyan.com>
2021-02-23 19:29:29 +0800

    JS兼容性处理

---
+++ 13-js兼容性处理/webpack.config.js
@@ -0,0 +1,64 @@
+const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build'),
+    publicPath:'./'
+  },
+  module: {
+    rules: [
+      /* 
+        js兼容性处理：babel-loader @babel-core
+        1. 基本js兼容性处理 --> @babel/preset-env
+          问题：只能转换基本语法，如promise高级语法不能转换
+        2. 全部js兼容性处理 --> @babel/polyfill
+          问题：只想解决部分兼容性问题，不是所有兼容性，导致代码全部引入，体积太大～
+        3. 需要做兼容性处理的：按需加载 --> core-js
+      */
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          // 预设：指示babel做怎么样的兼容性处理
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                // 按需加载
+                useBuiltIns: 'usage',
+                // 指定core-js版本
+                corejs: {
+                  version: 3
+                },
+                // 指定兼容性做到哪个版本浏览器
+                targets: {
+                  chrome: '60',
+                  firefox: '60',
+                  ie: '9',
+                  safari: '10',
+                  edge: '17'
+                }
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'built.html'
+    }),
+    new MiniCssExtractPlugin({
+      // 对输出的css文件进行重命名
+      // filename: 'css/built.css'
+    }),
+  ],
+  mode: 'development'
+}
